7021d1228bcc6b488a3e220fe9d4d3f683e0013d
Joshi <akanksha.joshi@siemens.com>
2023-07-07 16:15:54 +0100

    new commit

--- TestScript.sh
+++ TestScript.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 echo "Hello World"
+a=abc
+echo "$a"
+
+action=$1
+case $action in
+  a) echo -e "/e[32m Testing green/e[0m";;
+  b) echo  -e "/e[31m Testing red/e[0m" ;;
+  *) echo  -e "/e[33m Testing yellow/e[0m" ;;
+
+esac
+
 exit 0
