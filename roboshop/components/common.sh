646f86f146d4224daa2efb0bf1008a39c1c6b66c
Joshi <akanksha.joshi@siemens.com>
2023-07-08 17:09:22 +0100

    new commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -2,13 +2,13 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ] ; then
-    echo -e "\e[31m Please run the script with root user or sudo access \e[0m]"
+    echo -e "\e[31m Please run the script with root user or sudo access \e[0m"
 fi
 
 stat() {
     if [ $1 -ne 0 ] ; then
-        echo -e "\e[31m Failure \e[0m]"
+        echo -e "\e[31m Failure \e[0m"
     else
-        echo -e "\e[32m Success \e[0m]"
+        echo -e "\e[32m Success \e[0m"
     fi
 }
