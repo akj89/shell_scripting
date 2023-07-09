89c4d377dc735a669583c5ba7dc97b0521af88bf
Joshi <akanksha.joshi@siemens.com>
2023-07-09 18:11:54 +0100

    new commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 COMPONENT=mysql
+LOGFILE=/tmp/$COMPONENT.log
 
 source components/common.sh
 
 echo -n "Configuring $COMPONENT repo:"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE
 stat $?
 
 echo -n "Installing $COMPONENT component:"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>> $LOGFILE
 stat $?
 
 echo -n "Starting $COMPONENT service:"
-systemctl enable mysqld && systemctl start mysqld
+systemctl enable mysqld && systemctl start mysqld &>> $LOGFILE
 stat $?
 
 #echo -n "Changing default password:"
