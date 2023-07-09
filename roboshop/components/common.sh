d8848b07b0e22d9a47ac9e52b18fab7f796daf13
Joshi <akanksha.joshi@siemens.com>
2023-07-09 17:51:03 +0100

    new commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -12,3 +12,72 @@ stat() {
         echo -e "\e[32m Success \e[0m"
     fi
 }
+
+NODE_JS() {
+    echo -n "Downloading nodejs repository:"
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
+    stat $?
+
+    echo -n "Installing nodejs:"
+    yum install nodejs -y  &>> $LOGFILE
+    stat $?
+
+    # create user
+    CREATE_USER
+
+    # Doenload and extract
+    DOWNLOAD_AND_EXTRACT
+
+    echo -n "Installing the $COMPONENT dependencies:"
+    npm install &>> $LOGFILE
+    stat $?
+
+    # Configure services
+    CONFIGURE_SERVICE
+
+    #Start Service
+    START_SERVICE
+
+    echo -e "\e[32m _______________$COMPONENT Installation completed_____________\e[0m"
+}
+
+CREATE_USER() {
+    id $APPUSER &>> $LOGFILE
+    if [ $? -ne 0 ]; then
+        echo -n "Creating user $APPUSER :"
+        useradd $APPUSER &>> $LOGFILE
+        stat $?
+    fi
+}
+
+DOWNLOAD_AND_EXTRACT() {
+    echo -n "Downloading the $COMPONENT:"
+    curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>> $LOGFILE
+    stat $?
+
+    echo -n "Performing cleanup and unzipping the $COMPONENT:"
+    rm -rf /home/$APPUSER/$COMPONENT
+    cd /home/$APPUSER
+    unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE && mv -f $COMPONENT-main $COMPONENT &>> $LOGFILE
+    stat $?
+
+    cd /home/$APPUSER/$COMPONENT
+    echo -n "Changing permissions to $APPUSER"
+    chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT &&  chmod -R 775 /home/$APPUSER/$COMPONENT 
+    stat $?
+}
+
+CONFIGURE_SERVICE() {
+    echo -n "Configuring $COMPONENT file:"
+    sed -i -e 's/MONGO_DNSNAME/mongodb.ajrobot.co.uk/' /home/$APPUSER/$COMPONENT/systemd.service
+    stat $?
+
+    mv -f /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+}
+
+START_SERVICE() {
+    echo -n "Starting $COMPONENT service:"
+    systemctl daemon-reload &>> $LOGFILE
+    systemctl start $COMPONENT &>> $LOGFILE
+    stat $?
+}
