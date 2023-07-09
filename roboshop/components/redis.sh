381326df141f4f9843022061841f763c595e1578
Joshi <akanksha.joshi@siemens.com>
2023-07-09 16:28:36 +0100

    new commit

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+COMPONENT=redis
+
+source components/common.sh
+
+echo -n "Downloading $COMPONENT repository:"
+curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo  &>> $LOGFILE
+stat $?
+
+echo -n "Installing $COMPONENT:"
+yum install $COMPONENT-6.2.11 -y  &>> $LOGFILE
+stat $?
+
+echo -n "Configuring $COMPONENT file:"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/$COMPONENT.conf
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/$COMPONENT/$COMPONENT.conf
+
+echo -n "Starting $COMPONENT service:"
+systemctl daemon-reload &>> $LOGFILE
+systemctl enable $COMPONENT  &>> $LOGFILE
+systemctl start $COMPONENT  &>> $LOGFILE
+stat $?
